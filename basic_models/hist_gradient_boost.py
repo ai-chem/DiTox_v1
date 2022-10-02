d6a62462abb603d782dd7482a42f7ecc4de27563
kshiroky <kshiroky2002@gmail.com>
2022-10-02 20:04:11 +0300

    added two models and viability bins

--- basic_models/hist_gradient_boost.py
+++ basic_models/hist_gradient_boost.py
@@ -1,4 +1,4 @@
-from sklearn.ensemble import ExtraTreesRegressor
+from sklearn.ensemble import HistGradientBoostingRegressor
 from sklearn.metrics import r2_score, mean_squared_error
 from sklearn.model_selection import train_test_split, KFold
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
@@ -7,7 +7,7 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-path_to_data = "https://raw.githubusercontent.com/djzhendogg/predicting-toxicity-of-nanomaterials/master/with_z_3090.csv"
+path_to_data = "https://raw.githubusercontent.com/djzhendogg/predicting-toxicity-of-nanomaterials/master/last_df_3090.csv"
 dataset = pd.read_csv(path_to_data)
 
 dataset.drop(["Unnamed: 0", "Material", "Elements"], axis=1, inplace=True)
@@ -23,34 +23,8 @@ for feature in categ_features:
 print(dataset.describe())
 # print(Y.describe())
 
-correlation = dataset.corr()
-fig1 = plt.figure(figsize=(10, 6), constrained_layout=True)
-plt.title("Correlation matrix")
-sns.heatmap(correlation)
-# violin plots
-fig2, ax_ = plt.subplots(5, 4, figsize=(12, 11))
-ax = []
-for i in ax_:
-    ax += i.tolist()
-for number, column in enumerate(dataset.columns):
-    sns.violinplot(data=dataset, x=column, ax=ax[number])
-fig2.suptitle("Violin plots for columns in db")
-plt.subplots_adjust(left=0.1,
-                    bottom=0.1,
-                    right=0.9,
-                    top=0.9,
-                    wspace=0.3,
-                    hspace=0.6)
-
-# found outlying data in viability
-print("Outlying viability: \n", dataset["Viability (%)"][dataset["Viability (%)"] > 500])
 out_index = dataset[dataset["Viability (%)"] == dataset["Viability (%)"].max()].index
 dataset.drop(out_index, inplace=True)
-# violin plot for viability
-fig3 = plt.figure(figsize=(10, 6), constrained_layout=True)
-sns.violinplot(data=dataset["Viability (%)"], orient="h")
-plt.title("Violin plot for viability without outlier data")
-# plt.show()
 
 # split the features and the target
 label = {"Viability (%)"}
@@ -96,24 +70,24 @@ x_test = pd.DataFrame(test_num_features, columns=test_numerical_features.columns
 x_test = x_test.merge(X_test[categ_features], right_index=True, left_index=True)
 
 # assign and learn the model
-regressor = ExtraTreesRegressor()
+regressor = HistGradientBoostingRegressor()
 regressor.fit(x_train, Y_train)
 # estimate the model on the test data
 prediction = regressor.predict(x_test)
 print("Regressor R2-score: ", r2_score(Y_test, prediction))
 
 # feature importance
-importance = regressor.feature_importances_
-features = regressor.feature_names_in_
+# importance = regressor.feature_importances_
+# features = regressor.feature_names_in_
 
-fig4 = plt.figure(constrained_layout=True)
-indices = np.argsort(importance)
-feature_dict = {key: value for key, value in zip(features, importance)}
-feature_dictionary = pd.DataFrame(zip(features, importance * 100), columns=["Feature ID", "Importance"])
-feature_dictionary.sort_values(by="Importance", ascending=False, inplace=True)
-feature_dictionary.to_excel("random_forest_feature_importance.xlsx")
-plt.title("ExtraTreesRegressor feature importance")
-sns.barplot(feature_dictionary, x="Importance", y="Feature ID")
+# fig4 = plt.figure(constrained_layout=True)
+# indices = np.argsort(importance)
+# feature_dict = {key: value for key, value in zip(features, importance)}
+# feature_dictionary = pd.DataFrame(zip(features, importance * 100), columns=["Feature ID", "Importance"])
+# feature_dictionary.sort_values(by="Importance", ascending=False, inplace=True)
+# feature_dictionary.to_excel("random_forest_feature_importance.xlsx")
+# plt.title("HistGradientBoostingRegressor feature importance")
+# sns.barplot(feature_dictionary, x="Importance", y="Feature ID")
 
 
 # 10-fold cross-validation
@@ -144,7 +118,7 @@ for train_index, test_index in kf.split(X):
     x_test = pd.DataFrame(test_num_features, columns=test_numerical_features.columns, index=X_test.index)
     x_test = x_test.merge(X_test[categ_features], right_index=True, left_index=True)
 
-    new_regressor = ExtraTreesRegressor()
+    new_regressor = HistGradientBoostingRegressor()
     new_regressor.fit(x_train, y_train)
     train_pred = new_regressor.predict(x_train)
     test_pred = new_regressor.predict(x_test)
@@ -156,9 +130,9 @@ for train_index, test_index in kf.split(X):
 # visualize the information about the model
 fig5 = plt.figure()
 sns.scatterplot(x=Y_test, y=prediction)
-plt.title("Visualization of ExtraTreesRegressor prediction on test data")
+plt.title("Visualization of HistGradientBoostingRegressor prediction on test data")
 plt.show()
 
 print("leave-one-out cross-validation result (R2-score): ", r2_score(Y_test, prediction))
 print("10-fold cross-validation result (R2-score): ", np.mean(test_R2_metric_results))
-print("10-fold cross-validation result (RMSE): ", np.mean(test_rmse_metric_results))
+print("10-fold cross-validation result (RMSE): ", np.sqrt(np.mean(test_rmse_metric_results)))
--- statistics/stats.py
+++ statistics/stats.py
@@ -35,15 +35,53 @@ support_ds = dataset.copy()
 yes_coat_index = dataset[dataset['Coat'] > 0].index
 support_ds.loc[yes_coat_index, 'Coat'] = 1
 
+# 5 quantiles for viability
+quantile_values = [np.quantile(dataset["Viability (%)"].tolist(), quant / 10) for quant in range(5)]
+col_palette = sns.color_palette(n_colors=5)
+target = "Viability (%)"
+all_docs_number = []
+viability_plot1 = plt.figure()
+split_ds = None
+for number in range(len(quantile_values)):
+    # if the value is in the middle part of dataset
+    # we need to consider left and right borders of bins
+    if number < len(quantile_values) - 1:
+        left_border = quantile_values[number]
+        right_border = quantile_values[number + 1]
+        doc_bin = dataset[dataset[target] >= left_border]
+        doc_bin.drop(doc_bin[doc_bin[target] >= right_border].index, inplace=True)
+    # if the values are in the end of the dataset
+    # we consider only the left border
+    else:
+        left_border = quantile_values[number]
+        doc_bin = dataset[dataset[target] >= left_border]
+    # encode the target value with the number of bin
+    # to avoid target information leakage
+    doc_bin[target] = number
+    all_docs_number.append(doc_bin[target].count())
+    # fill the supporting dataset with created bins
+    if split_ds is None:
+        split_ds = pd.DataFrame(doc_bin, columns=dataset.columns)
+    else:
+        split_ds = pd.concat([split_ds, doc_bin])
+
+split_ds.rename(columns={"Viability (%)": "Viability quantiles"}, inplace=True)
+
 tsne = TSNE(perplexity=40)
 dec_ds = tsne.fit_transform(dataset)
 dec_ds = pd.DataFrame(dec_ds, columns=['component_1', 'component_2'])
 
 color_palt = sns.color_palette(n_colors=len(ds['Material'].unique()))
 
-fig_tsne, ax_ = plt.subplots(2, 1, figsize=(30, 20))
-sns.scatterplot(x=dec_ds['component_1'], y=dec_ds['component_2'], hue=ds['Material'], palette=color_palt, ax=ax_[0])
-sns.scatterplot(x=dec_ds['component_1'], y=dec_ds['component_2'], hue=support_ds['Coat'], palette='bright', ax=ax_[1])
+fig_tsne_0= plt.figure()
+sns.scatterplot(x=dec_ds['component_1'], y=dec_ds['component_2'], hue=ds['Material'], palette=color_palt)
+
+fig_tsne_1= plt.figure()
+sns.scatterplot(x=dec_ds['component_1'], y=dec_ds['component_2'], hue=support_ds['Coat'], palette='bright')
+
+fig_tsne_2= plt.figure()
+sns.scatterplot(x=dec_ds['component_1'], y=dec_ds['component_2'], hue=split_ds['Viability quantiles'], palette='bright')
+
 plt.show()
 
 xticks = range(0, 3090, 400)
